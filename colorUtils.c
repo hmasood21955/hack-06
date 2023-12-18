0024547eb2f97fdd52888b5e85562f33b876c5ed
hmasood21955 <hmasood878@gmail.com>
2023-12-18 19:29:33 +0500

    Update colorUtils.c

--- colorUtils.c
+++ colorUtils.c
@@ -4,3 +4,13 @@ int rgbToCmyk(int r, int g, int b, double* c, double* m, double* y, double* k) {
     if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255) {
         return 1;
     }
+
+    if (r == 0 && g == 0 && b == 0) {
+        *c = 0;
+        *m = 0;
+        *y = 0;
+        *k = 1;
+    } else {
+        *c = 1 - (double)r / 255;
+        *m = 1 - (double)g / 255;
+        *y = 1 - (double)b / 255;
