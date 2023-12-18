955a4c9358ac514f49f8c5731a6e78b99b70a3b8
hmasood21955 <hmasood878@gmail.com>
2023-12-18 18:54:14 +0500

    headerfile for CMYK to RGB

--- colorUtils.h
+++ colorUtils.h
@@ -2,3 +2,4 @@
 #define COLORUTILS_H
 
 int rgbToCmyk(int r, int g, int b, double* c, double* m, double* y, double* k);
+int cmykToRgb(double c, double m, double y, double k, int* r, int* g, int* b);
